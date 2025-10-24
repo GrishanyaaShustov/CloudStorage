db7958102d7f549210912a4fc1df74eee459e03f
Grisha <dinsuil275283@gmail.com>
2025-10-24 20:03:26 +0500

    Added "create folder" endpoint

--- services/user-service/src/main/java/cloud/storage/userservice/repository/FolderRepository.java
+++ services/user-service/src/main/java/cloud/storage/userservice/repository/FolderRepository.java
@@ -1,9 +1,14 @@
 package cloud.storage.userservice.repository;
 
 import cloud.storage.userservice.models.Folder;
+import cloud.storage.userservice.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FolderRepository extends JpaRepository<Folder, Long> {
+    Optional<Folder> findFolderById(Long id);
+    boolean existsByNameAndParentAndUser(String name, Folder parent, User user);
 }
