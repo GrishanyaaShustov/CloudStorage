a0a37c42869f29aa3a1bb56aedcf5b3c453e49e9
Grisha <dinsuil275283@gmail.com>
2025-10-25 10:24:35 +0500

    delete old methods

--- services/storage-service/src/main/java/cloud/storage/storageservice/services/StorageGrpcService.java
+++ services/storage-service/src/main/java/cloud/storage/storageservice/services/StorageGrpcService.java
@@ -6,11 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.grpc.server.service.GrpcService;
-import com.google.protobuf.ByteString;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 
 @GrpcService
 @RequiredArgsConstructor
@@ -18,32 +13,6 @@ public class StorageGrpcService extends StorageServiceGrpc.StorageServiceImplBas
 
     private static final Logger log = LoggerFactory.getLogger(StorageGrpcService.class);
     private final S3ProviderService s3ProviderService;
-
-    @Override
-    public void uploadFile(UploadFileRequest request, StreamObserver<UploadFileResponse> responseObserver) {
-        log.debug("Upload request received for key: {}", request.getKey());
-        try {
-            s3ProviderService.uploadFile(
-                    request.getKey(),
-                    new ByteArrayInputStream(request.getFileData().toByteArray()),
-                    request.getFileData().size(),
-                    request.getContentType()
-            );
-            log.info("File successfully uploaded: {}", request.getKey());
-            responseObserver.onNext(UploadFileResponse.newBuilder()
-                    .setSuccess(true)
-                    .setMessage("File uploaded successfully")
-                    .build());
-        } catch (Exception e) {
-            log.error("Failed to upload file with key '{}': {}", request.getKey(), e.getMessage(), e);
-            responseObserver.onNext(UploadFileResponse.newBuilder()
-                    .setSuccess(false)
-                    .setMessage("Error: " + e.getMessage())
-                    .build());
-        } finally {
-            responseObserver.onCompleted();
-        }
-    }
 
     @Override
     public void deleteFile(DeleteFileRequest request, StreamObserver<DeleteFileResponse> responseObserver) {
@@ -81,41 +50,6 @@ public class StorageGrpcService extends StorageServiceGrpc.StorageServiceImplBas
             responseObserver.onNext(CopyFileResponse.newBuilder()
                     .setSuccess(false)
                     .setMessage("Error copying file: " + e.getMessage())
-                    .build());
-        } finally {
-            responseObserver.onCompleted();
-        }
-    }
-
-    @Override
-    public void downloadFile(DownloadFileRequest request, StreamObserver<DownloadFileResponse> responseObserver) {
-        log.debug("Download request received for key: {}", request.getKey());
-        try {
-            InputStream inputStream = s3ProviderService.downloadFile(request.getKey());
-
-            // Читаем InputStream в byte[]
-            ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-            byte[] data = new byte[4096];
-            int nRead;
-            while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
-                buffer.write(data, 0, nRead);
-            }
-            buffer.flush();
-            byte[] fileBytes = buffer.toByteArray();
-
-            log.info("File successfully downloaded: {}, size: {} bytes", request.getKey(), fileBytes.length);
-
-            responseObserver.onNext(DownloadFileResponse.newBuilder()
-                    .setSuccess(true)
-                    .setMessage("File downloaded successfully")
-                    .setFileData(ByteString.copyFrom(fileBytes))
-                    .build());
-
-        } catch (Exception e) {
-            log.error("Failed to download file with key '{}': {}", request.getKey(), e.getMessage(), e);
-            responseObserver.onNext(DownloadFileResponse.newBuilder()
-                    .setSuccess(false)
-                    .setMessage("Error downloading file: " + e.getMessage())
                     .build());
         } finally {
             responseObserver.onCompleted();
